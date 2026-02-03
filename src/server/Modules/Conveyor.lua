8eb6f614a731f2f446a2d802079a223ba5f2da9c
Frederikhome <Frederik.plum@live.dk>
2026-02-03 21:27:56 +0100

    Created types for the server

--- src/server/Modules/Conveyor.lua
+++ src/server/Modules/Conveyor.lua
@@ -1,23 +1,74 @@
-local Conveyor = require("../Conveyor")
+local HttpService = game:GetService("HttpService")
 local Types = require("../Types")
+local Math = require("../Math")
 
 local Conveyor = {}
 Conveyor.__index = Conveyor
 
-function Conveyor.new(part: BasePart, speed: number): Types.Conveyor
+function Conveyor.new(part: BasePart, speed: number, target: Types.Container): Types.Conveyor
 	local self = {
+		Id = HttpService:GenerateGUID(false),
 		Part = part,
 		Bags = {},
 		Speed = speed,
 		BagEvents = {},
+		Target = target,
 		POI = {},
 	}
 
 	return setmetatable(self, Conveyor) :: any
 end
 
-function Conveyor.add(self: Types.Conveyor, bag: Types.Bag) end
+function Conveyor.add(self: Types.Conveyor, bag: Types.Bag)
+	if bag.Container then
+		error("Add bag cannot add a bag that already has a container... Please move the bag instead.")
+	end
 
-function Conveyor.remove(self: Types.Conveyor, bag: Types.Bag) end
+	bag.Container = self
+	table.insert(self.Bags, bag)
 
-function Conveyor.update(self: Types.Conveyor) end
+	bag.StudsTraversed = 0
+	bag.LastUpdate = tick()
+
+	--Update this function IF POI are added
+	self.BagEvents[bag] = tick() + Math.TimeRemaining(self, bag.StudsTraversed)
+end
+
+function Conveyor.remove(self: Types.Conveyor, bag: Types.Bag)
+	if not bag.Container then
+		error("The given bag did not have a container")
+	end
+
+	if bag.Container ~= self then
+		error("Cannot remove bags from other containers")
+	end
+
+	self.BagEvents[bag] = nil
+
+	local i = table.find(self.Bags, bag)
+	if not i then
+		error("The bag was for some reason already removed from the bag table..")
+	end
+	table.remove(self.Bags, i)
+end
+
+function Conveyor.update(self: Types.Conveyor)
+	for bag, t in self.BagEvents do
+		if tick() <= t then
+			--Reached destination
+			--Update this to handle POI if they are ever added
+
+			bag:move(self.Target)
+		end
+	end
+end
+
+function Conveyor.serialize(self: Types.Conveyor): Types.SerializedConveyor
+	return {
+		Speed = self.Speed,
+		Target = self.Target:serialize(),
+		Type = "Conveyor",
+		Id = self.Id,
+		POI = {},
+	}
+end
